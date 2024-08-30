cf01170b455e59e364eb4569070341d94d2fb4ca
Simon Bihel <simon.bihel@spruceid.com>
2024-08-30 17:29:23 +0100

    End-to-end test with valid mdoc

--- Targets/AppUIKit/Sources/ScanView.swift
+++ Targets/AppUIKit/Sources/ScanView.swift
@@ -1,6 +1,6 @@
 import CoreBluetooth
 import SwiftUI
-import SpruceIDWalletSdk
+import SpruceIDMobileSdk
 
 extension String: Identifiable {
     public typealias ID = Int
@@ -13,10 +13,28 @@ public struct ScanView: View {
     @State private var scanned: String?
 
     public var body: some View {
+        let issuer_cert = """
+            -----BEGIN CERTIFICATE-----
+            MIIChjCCAiygAwIBAgIUPgwgeCSsRYiU8iN6KHqKED3w/AAwCgYIKoZIzj0EAwIw
+            bjELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAk5ZMRswGQYDVQQKDBJTcHJ1Y2VJRCBU
+            ZXN0IFJvb3QxNTAzBgNVBAMMLFNwcnVjZUlEIFRlc3QgQ2VydGlmaWNhdGUgUm9v
+            dCBPSUQ0VkNJV2FsbGV0MB4XDTI0MDgzMDE0MzQyM1oXDTM0MDgyODE0MzQyM1ow
+            bjELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAk5ZMRswGQYDVQQKDBJTcHJ1Y2VJRCBU
+            ZXN0IFJvb3QxNTAzBgNVBAMMLFNwcnVjZUlEIFRlc3QgQ2VydGlmaWNhdGUgUm9v
+            dCBPSUQ0VkNJV2FsbGV0MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEPX/59+YO
+            meAfOugnUqfRi8TCHD1GC/R2Xip83aNdmM9DVP7NFiURpxIgBNDmj4VZodDoiz/B
+            GSO9l6Sypv1hYKOBpzCBpDAdBgNVHQ4EFgQU5xn7tyLS9unEonXA5D2Jm/9ERcAw
+            EgYDVR0TAQH/BAgwBgEB/wIBADA+BgNVHR8ENzA1MDOgMaAvhi1odHRwczovL2lu
+            dGVyb3BldmVudC5zcHJ1Y2VpZC5jb20vaW50ZXJvcC5jcmwwDgYDVR0PAQH/BAQD
+            AgEGMB8GA1UdEgQYMBaBFGludGVyb3BAc3BydWNlaWQuY29tMAoGCCqGSM49BAMC
+            A0gAMEUCIQCqFHqCM5NTgkiSJbOeFvGKJKBbordnzOVzb7UrFGQL5gIgKgh9gMKh
+            VixVit4VpJnYkcJXhQpTba/kWPCCfJh06kU=
+            -----END CERTIFICATE-----
+            """
         QRCodeScanner(onRead: onRead, onCancel: onCancel)
             .sheet(item: $scanned, onDismiss: onCancel) {
                 scanned in
-                MDocReaderView(uri: scanned, requestedItems: ["org.iso.18013.5.1": ["given_name": true]])
+                MDocReaderView(uri: scanned, requestedItems: ["org.iso.18013.5.1": ["given_name": true]], trustAnchorRegistry: [issuer_cert])
             }
     }
 
@@ -34,8 +52,8 @@ public struct MDocReaderView: View {
     @StateObject var delegate: MDocScanViewDelegate
     @Environment(\.presentationMode) var presentationMode
     
-    init(uri: String, requestedItems: [String: [String: Bool]]) {
-        self._delegate = StateObject(wrappedValue: MDocScanViewDelegate(uri: uri, requestedItems: requestedItems))
+    init(uri: String, requestedItems: [String: [String: Bool]], trustAnchorRegistry: [String]?) {
+        self._delegate = StateObject(wrappedValue: MDocScanViewDelegate(uri: uri, requestedItems: requestedItems, trustAnchorRegistry: trustAnchorRegistry))
     }
     
     public var body: some View {
@@ -104,8 +122,8 @@ class MDocScanViewDelegate: ObservableObject {
     @Published var state: BLEReaderSessionState = .advertizing
     private var mdocReader: MDocReader?
     
-    init(uri: String, requestedItems: [String: [String: Bool]]) {
-        self.mdocReader = MDocReader(callback: self, uri: uri, requestedItems: requestedItems)
+    init(uri: String, requestedItems: [String: [String: Bool]], trustAnchorRegistry: [String]?) {
+        self.mdocReader = MDocReader(callback: self, uri: uri, requestedItems: requestedItems, trustAnchorRegistry: trustAnchorRegistry)
     }
     
     func cancel() {
