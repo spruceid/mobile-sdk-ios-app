24c729fc57e52d470ded02a1054eab296ca3676d
Juliano Cezar Chagas Tavares <julianocezargyn@hotmail.com>
2024-10-04 15:28:17 -0300

    Include vcplayground contexts

--- Targets/AppUIKit/Sources/wallet/OID4VCI.swift
+++ Targets/AppUIKit/Sources/wallet/OID4VCI.swift
@@ -5,8 +5,8 @@ import SpruceIDMobileSdkRs
 struct OID4VCI: Hashable {}
 
 struct OID4VCIView: View {
-
     @State var credential: String?
+    @State var credentialPack: CredentialPack?
 
     @Binding var path: NavigationPath
     
@@ -45,9 +45,15 @@ struct OID4VCIView: View {
                     signature: Data(Data(signature!).base64EncodedUrlSafe.utf8)
                 )
                 
-                let credential = try await oid4vciSession.exchangeCredential(proofsOfPossession: [pop])
-                credential.forEach {
-                    self.credential = String(decoding: Data($0.payload), as: UTF8.self)
+                try oid4vciSession.setContextMap(values: getVCPlaygroundOID4VCIContext())
+                
+                self.credentialPack = CredentialPack()
+                let credentials = try await oid4vciSession.exchangeCredential(proofsOfPossession: [pop])
+                
+                try credentials.forEach {
+                    let cred = String(decoding: Data($0.payload), as: UTF8.self)
+                    _ = try self.credentialPack!.addW3CVC(credentialString: cred)
+                    self.credential = cred
                 }
                 
             } catch {
@@ -79,4 +85,37 @@ struct OID4VCIView: View {
         }
 
     }
+}
+
+func getVCPlaygroundOID4VCIContext() throws -> [String: String] {
+    var context: [String: String] = [:]
+    
+    var path = Bundle.main.path(forResource: "contexts.vcplayground.org_examples_alumni_v1", ofType: "json")
+    context["https://contexts.vcplayground.org/examples/alumni/v1.json"] = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+    
+    path = Bundle.main.path(forResource: "w3id.org_first-responder_v1", ofType: "json")
+    context["https://w3id.org/first-responder/v1"] = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+    
+    path = Bundle.main.path(forResource: "w3id.org_vdl_aamva_v1", ofType: "json")
+    context["https://w3id.org/vdl/aamva/v1"] = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+    
+    path = Bundle.main.path(forResource: "w3id.org_citizenship_v3", ofType: "json")
+    context["https://w3id.org/citizenship/v3"] = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+    
+    path = Bundle.main.path(forResource: "contexts.vcplayground.org_examples_movie-ticket_v1", ofType: "json")
+    context["https://contexts.vcplayground.org/examples/movie-ticket/v1.json"] = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+
+    path = Bundle.main.path(forResource: "purl.imsglobal.org_spec_ob_v3p0_context-3.0.2", ofType: "json")
+    context["https://purl.imsglobal.org/spec/ob/v3p0/context-3.0.2.json"] = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+    
+    path = Bundle.main.path(forResource: "contexts.vcplayground.org_examples_food-safety-certification_v1", ofType: "json")
+    context["https://contexts.vcplayground.org/examples/food-safety-certification/v1.json"] = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+    
+    path = Bundle.main.path(forResource: "contexts.vcplayground.org_examples_gs1-8110-coupon_v2", ofType: "json")
+    context["https://contexts.vcplayground.org/examples/gs1-8110-coupon/v2.json"] = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+    
+    path = Bundle.main.path(forResource: "contexts.vcplayground.org_examples_customer-loyalty_v1", ofType: "json")
+    context["https://contexts.vcplayground.org/examples/customer-loyalty/v1.json"] = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+    
+    return context
 }
